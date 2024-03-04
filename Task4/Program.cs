7c0eefa58b6b6ee62ae785e204158f5560ec28fb
Sergey <rusackov.serega@yandex.ru>
2024-03-04 20:58:09 +0300

    add Task6

---
+++ Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и 
+//выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+
+
+
+
+
+Console.WriteLine("Enter three number :");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a < b)
+    Console.WriteLine("Max = {0}", b);
+  
+else if (a > b)
+    Console.WriteLine("Max = {0}", a);
+
+else if (b > c) 
+    Console.WriteLine("Max = {0}", b); 
+
+if (b < c)
+    Console.WriteLine("Max = {0}", c);
+    
+Console.WriteLine();
---
+++ Task6/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+System.Console.WriteLine ("Enter number :");
+
+string number = Console.ReadLine();
+int number1 = int.Parse (number);
+
+
+if (number1 % 2 == 1)
+
+    Console.WriteLine($"Number {number1} is odd");
+
+else
+
+    Console.WriteLine($"Number {number1} is even");
